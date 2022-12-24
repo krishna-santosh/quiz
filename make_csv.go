10f4f938cc59e74f0f2b4aa80bcbb436ff3fa20b
Krishna Santosh <75202541+krishna-santosh@users.noreply.github.com>
2022-12-24 10:52:23 +0530

    initial commit

---
+++ make_csv.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"crypto/sha256"
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func makeCSV() {
+	fmt.Println("CSV Generator")
+	fmt.Print("What do you want you CSV file to be called: ")
+
+	fileName := getInput()
+
+	file, err := os.Create(strings.TrimSpace(fileName))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// CSV writer
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	fmt.Print("Enter the number of questions: ")
+	qcount, err := strconv.Atoi(getInput())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("Would you like to hash your answers? [Y/n]: ")
+	hashResp := getInput()
+	toHash := true
+	if hashResp == "N" || hashResp == "n" {
+		toHash = false
+	}
+
+	for i := 1; i <= qcount; i++ {
+		r := questin_answer_input(i, toHash)
+		err := writer.Write(r)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func questin_answer_input(question_no int, hash bool) []string {
+	fmt.Printf("Question %d: ", question_no)
+	qinput := getInput()
+
+	fmt.Print("Answer : ")
+	ainput := getInput()
+
+	if hash {
+		ainput = fmt.Sprintf("%x", sha256.Sum256([]byte(ainput)))
+	}
+
+	return []string{qinput, ainput}
+}
+
+func getInput() string {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	return strings.TrimSpace(input)
+}
